5543e6e80d02a6aa1fe8d4139661ce0c4be3fb7d
youngskooter <moshekaruru@gmail.com>
2026-08-09 13:11:19 +0300

    added correct backend healthcheck

--- backend/src/app.js
+++ backend/src/app.js
@@ -12,7 +12,7 @@ const app = express();
 app.use(cors());
 app.use(express.json());
 
-app.use("/health", healthRoutes);
+app.use("/api/health", healthRoutes);
 app.use("/api/tasks", taskRoutes);
 
 export default app;
